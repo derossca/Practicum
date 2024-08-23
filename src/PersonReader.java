642e5a7c164eb1a1a09c4280d7dbc33f565c2b1d
derossca <derossca@mail.uc.edu>
2024-08-23 14:01:42 -0400

    experimenting with columnar formatting

--- src/PersonReader.java
+++ src/PersonReader.java
@@ -1,51 +1,69 @@
 import javax.swing.*;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Scanner;
+
+import static java.nio.file.StandardOpenOption.CREATE;
+
 public class PersonReader {
-    public static void  main(String[] args)
+    public static void main(String[] args)
     {
-        //Declar variables
         JFileChooser chooser = new JFileChooser();
-        Scanner inFile;
-        String line;
-        Path target = new File(System.getProperty("user.dir")).toPath();
-        target = target.resolve("src");
-        //set the chooser to the project src directory
-        chooser.setCurrentDirectory(target.toFile());
+        File selectedFile;
+        String rec = "";
 
-        try //code that might trigger exception goes here
-        {
-            if(chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION)
-            {
-                target = chooser.getSelectedFile().toPath();
+        try {
+            // uses a fixed known path:
+            //  Path file = Paths.get("c:\\My Documents\\data.txt");
 
-                inFile = new Scanner(target);
+            // use the toolkit to get the current working directory of the IDE
+            // Not sure if the toolkit is thread safe...
+            File workingDirectory = new File(System.getProperty("user.dir"));
 
-                while(inFile.hasNext())
-                {
-                    line = inFile.nextLine();
-                    System.out.println(line);
+            // Typiacally, we want the user to pick the file so we use a file chooser
+            // kind of ugly code to make the chooser work with NIO.
+            // Because the chooser is part of Swing it should be thread safe.
+            chooser.setCurrentDirectory(workingDirectory);
+            // Using the chooser adds some complexity to the code.
+            // we have to code the complete program within the conditional return of
+            // the filechooser because the user can close it without picking a file
+
+            if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+                selectedFile = chooser.getSelectedFile();
+                Path file = selectedFile.toPath();
+                // Typical java pattern of inherited classes
+                // we wrap a BufferedWriter around a lower level BufferedOutputStream
+                InputStream in =
+                        new BufferedInputStream(Files.newInputStream(file, CREATE));
+                BufferedReader reader =
+                        new BufferedReader(new InputStreamReader(in));
+
+                // Finally we can read the file LOL!
+
+
+                int line = 0;
+                while (reader.ready()) {
+                    rec = reader.readLine();
+                    line++;
+                    // echo to screen
+                    System.out.printf("\n%-60s ", rec);
                 }
+                reader.close(); // must close the file to seal it and flush buffer
+                System.out.println("\n\nData file read!");
 
-                inFile.close();
-            }
-            else
+
+            } else  // User closed the chooser without selecting a file
             {
-                System.out.println("Sorry, you must select a file! Terminating!");
-                System.exit(0);
+                System.out.println("No file selected!!! ... exiting.\nRun the program again and select a file.");
             }
         }
         catch (FileNotFoundException e)
         {
-            System.out.println("File Not Found Error");
+            System.out.println("File not found!!!");
             e.printStackTrace();
         }
         catch (IOException e)
         {
-            System.out.println("IOException Erorr");
             e.printStackTrace();
         }
     }
